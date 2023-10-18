dce261762b56c97d9bb1c374c73f3f642fea647f
Isaac <carlin_isaac@hotmail.com>
2023-10-17 21:09:38 -0300

    CHORE: conexion con la BD produccion

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,20 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize("chatbot", "postgres", "admin", {
-  host: "localhost",
+// const sequelize = new Sequelize("chatbot", "postgres", "admin", {
+//   host: "localhost",
+//   dialect: "postgres",
+//   port: '5434'
+// });
+
+const sequelize = new Sequelize("chatbot-api", "fl0user", "FbTVqRy16YOw", {
+  host: "ep-odd-grass-06468340.us-east-2.aws.neon.fl0.io",
   dialect: "postgres",
-  port: '5434'
+  port: '5432',
+  dialectOptions: {
+    "ssl": {
+       "require": true
+    }
+  }
 });
 
 module.exports = sequelize;
