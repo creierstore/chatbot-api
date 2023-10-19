f3a89ed7f8d8d8ddae304531320da684c20f4499
pedro <pedro.jcaceresl@gmail.com>
2023-10-19 14:48:10 -0300

    Add: Añadido update y create para webhooks
    Al añadir un producto en shopify ahora, se añade también a la base datos
    Y al actualizar un producto en shopify, se actualiza en la BD.

--- src/services/producto.service.js
+++ src/services/producto.service.js
@@ -1,5 +1,8 @@
 const Producto = require("../models/producto.models");
 
+const htmlToText = require('html-to-text');
+
+
 const searchProducto = async (data) => {
     console.log("DATA SERVICE", { data });
 
@@ -15,6 +18,87 @@ const searchProducto = async (data) => {
   }
 };
 
+const createPoducto = async (producto) => {
+  const { title, price, description, image, productId } = producto;
+  try {
+    const newData = await Producto.create({
+      title,
+      price,
+      description,
+      image,
+      productId
+      // categoriaId,
+    });
+
+    return newData;
+
+  } catch (error) {
+    console.log("Unable to create product: ", error);
+  }
+}
+
+const isProductInDB = async (idProduct) => {
+  try {
+    const data = await Producto.findOne({
+      where: {
+        idProduct,
+      },
+    });
+
+    if (data?.dataValues?.id) {
+      return true;
+    }
+    return false;
+
+  } catch (error) {
+    console.log("Error", error)
+  }
+}
+
+
+const updateProducto = async (producto, idProduct) => {
+  try {
+    const data = await Producto.findOne({
+      where: {
+        idProduct,
+      },
+    });
+
+    // Transform the HTML description to plain text before updating
+    if (producto.description) {
+      producto.description = transformHtmlToPlainText(producto.description);
+    }
+
+    data.set(producto);
+
+    const updatedProduct = await data.save();
+
+    if (updatedProduct) {
+      console.log("Product updated!", updatedProduct);
+    }
+
+    return updatedProduct;
+
+  } catch (error) {
+    console.log("Error: ", error.message);
+  }
+};
+
+
+function transformHtmlToPlainText(htmlDescription) {
+  // Convert HTML to plain text and remove any HTML tags
+  const plainText = htmlToText.htmlToText(htmlDescription, {
+    wordwrap: 130, // Set the desired line width
+    ignoreImage: true, // Exclude images from the text
+  });
+
+  return plainText;
+}
+
 module.exports = {
   searchProducto,
+  createPoducto,
+  isProductInDB,
+  updateProducto,
+  transformHtmlToPlainText
 };
